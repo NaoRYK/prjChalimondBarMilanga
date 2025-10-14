7331b4048c22e9910f462a57a7483827f57bc0e6
Nao <92193274+NaoRYK@users.noreply.github.com>
2025-10-14 17:21:46 -0300

    Agregar archivos de proyecto.

---
+++ prjChalimondBarMilanga/frmVentas.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace prjChalimondBarMilanga
+{
+    partial class frmVentas
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dgvDatos = new DataGridView();
+            Comidas = new DataGridViewTextBoxColumn();
+            BebidasSinAlcohol = new DataGridViewTextBoxColumn();
+            BebidasConAlcohol = new DataGridViewTextBoxColumn();
+            Postres = new DataGridViewTextBoxColumn();
+            btnValidar = new Button();
+            btnMejorMozo = new Button();
+            btnTotal = new Button();
+            txtResultados = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)dgvDatos).BeginInit();
+            SuspendLayout();
+            // 
+            // dgvDatos
+            // 
+            dgvDatos.AllowUserToAddRows = false;
+            dgvDatos.AllowUserToDeleteRows = false;
+            dgvDatos.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvDatos.Columns.AddRange(new DataGridViewColumn[] { Comidas, BebidasSinAlcohol, BebidasConAlcohol, Postres });
+            dgvDatos.Location = new Point(12, 24);
+            dgvDatos.Name = "dgvDatos";
+            dgvDatos.Size = new Size(443, 172);
+            dgvDatos.TabIndex = 0;
+            // 
+            // Comidas
+            // 
+            Comidas.HeaderText = "Comidas";
+            Comidas.Name = "Comidas";
+            // 
+            // BebidasSinAlcohol
+            // 
+            BebidasSinAlcohol.HeaderText = "Bebidas sin alcohol";
+            BebidasSinAlcohol.Name = "BebidasSinAlcohol";
+            // 
+            // BebidasConAlcohol
+            // 
+            BebidasConAlcohol.HeaderText = "Bebidas con alcohol";
+            BebidasConAlcohol.Name = "BebidasConAlcohol";
+            // 
+            // Postres
+            // 
+            Postres.HeaderText = "Postres";
+            Postres.Name = "Postres";
+            // 
+            // btnValidar
+            // 
+            btnValidar.Location = new Point(12, 212);
+            btnValidar.Name = "btnValidar";
+            btnValidar.Size = new Size(443, 40);
+            btnValidar.TabIndex = 1;
+            btnValidar.Text = "Validar datos";
+            btnValidar.UseVisualStyleBackColor = true;
+            btnValidar.Click += btnValidar_Click;
+            // 
+            // btnMejorMozo
+            // 
+            btnMejorMozo.Location = new Point(12, 258);
+            btnMejorMozo.Name = "btnMejorMozo";
+            btnMejorMozo.Size = new Size(220, 45);
+            btnMejorMozo.TabIndex = 2;
+            btnMejorMozo.Text = "Mejor mozo";
+            btnMejorMozo.UseVisualStyleBackColor = true;
+            btnMejorMozo.Click += btnMejorMozo_Click;
+            // 
+            // btnTotal
+            // 
+            btnTotal.Location = new Point(238, 258);
+            btnTotal.Name = "btnTotal";
+            btnTotal.Size = new Size(217, 45);
+            btnTotal.TabIndex = 3;
+            btnTotal.Text = "Totales";
+            btnTotal.UseVisualStyleBackColor = true;
+            btnTotal.Click += btnTotal_Click;
+            // 
+            // txtResultados
+            // 
+            txtResultados.Location = new Point(12, 309);
+            txtResultados.Multiline = true;
+            txtResultados.Name = "txtResultados";
+            txtResultados.Size = new Size(443, 150);
+            txtResultados.TabIndex = 4;
+            // 
+            // frmVentas
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(481, 494);
+            Controls.Add(txtResultados);
+            Controls.Add(btnTotal);
+            Controls.Add(btnMejorMozo);
+            Controls.Add(btnValidar);
+            Controls.Add(dgvDatos);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Name = "frmVentas";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Ventas";
+            Load += frmVentas_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvDatos).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dgvDatos;
+        private DataGridViewTextBoxColumn Comidas;
+        private DataGridViewTextBoxColumn BebidasSinAlcohol;
+        private DataGridViewTextBoxColumn BebidasConAlcohol;
+        private DataGridViewTextBoxColumn Postres;
+        private Button btnValidar;
+        private Button btnMejorMozo;
+        private Button btnTotal;
+        private TextBox txtResultados;
+    }
+}
---
+++ prjChalimondBarMilanga/frmVentas.cs
@@ -0,0 +1,125 @@
+using System.Text;
+
+namespace prjChalimondBarMilanga
+{
+    public partial class frmVentas : Form
+    {
+        public frmVentas()
+        {
+            InitializeComponent();
+        }
+        string[] mozos = { "Julio", "Esteban", "Javier", "Gonzalo", "Alberto" };
+        bool datosValidados = false;
+        float[,] valoresGrilla;
+        private void frmVentas_Load(object sender, EventArgs e)
+        {
+            foreach (string mozo in mozos)
+            {
+                dgvDatos.Rows.Add(mozo);
+            }
+            valoresGrilla = new float[dgvDatos.Rows.Count, dgvDatos.Columns.Count];
+            btnMejorMozo.Enabled = datosValidados;
+            btnTotal.Enabled = datosValidados;
+        }
+
+        private void btnValidar_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < dgvDatos.Rows.Count; i++)
+            {
+                if (dgvDatos.Rows[i].IsNewRow) continue; // evito fila vacía
+
+
+                for (int j = 1; j < dgvDatos.Columns.Count; j++)
+                {
+
+
+                    object valorCelda = dgvDatos.Rows[i].Cells[j].Value;
+                    if (float.TryParse(Convert.ToString(valorCelda), out float resultado))
+                    {
+                        valoresGrilla[i, j] = resultado;
+                    }
+                    else
+                    {
+                        MessageBox.Show($"El valor en fila {i + 1}, columna {j + 1} no es un número válido.");
+                        return;
+                    }
+
+
+
+                }
+            }
+            datosValidados = true;
+            btnMejorMozo.Enabled = datosValidados;
+            btnTotal.Enabled = datosValidados;
+
+        }
+
+        private void btnMejorMozo_Click(object sender, EventArgs e)
+        {
+            if (valoresGrilla == null)
+            {
+                MessageBox.Show("Primero validá los datos con el botón 'Validar'.");
+                return;
+            }
+
+            float maxTotal = float.MinValue;
+            string mejorMozo = "";
+
+            for (int i = 0; i < mozos.Length; i++)
+            {
+                float suma = 0;
+
+                for (int j = 1; j < dgvDatos.Columns.Count; j++)
+                {
+                    suma += valoresGrilla[i, j];
+                }
+                if (suma > maxTotal)
+                {
+                    maxTotal = suma;
+                    mejorMozo = mozos[i];
+                }
+            }
+
+            txtResultados.Text = mejorMozo + " Con un aporte de $" + maxTotal.ToString("0.00"); ;
+
+        }
+
+        private void btnTotal_Click(object sender, EventArgs e)
+        {
+            if (valoresGrilla == null)
+            {
+                MessageBox.Show("Primero validá los datos con el botón 'Validar'.");
+                return;
+            }
+
+            int cantCategorias = dgvDatos.Columns.Count - 1; // 
+            float[] totalesPorCategoria = new float[cantCategorias];
+            float totalGeneral = 0;
+
+
+            for (int i = 0; i < mozos.Length; i++)
+            {
+                for (int j = 1; j < dgvDatos.Columns.Count; j++)
+                {
+                    float valor = valoresGrilla[i, j];
+                    totalesPorCategoria[j - 1] += valor;
+                    totalGeneral += valor;
+                }
+            }
+
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(" Totales por categoría:");
+
+            for (int k = 0; k < cantCategorias; k++)
+            {
+                sb.AppendLine($"Categoría {k + 1}: {totalesPorCategoria[k]:0.00}");
+            }
+
+            sb.AppendLine();
+            sb.AppendLine($"Total general vendido: {totalGeneral:0.00}");
+
+            txtResultados.Text = sb.ToString();
+        }
+    }
+}
